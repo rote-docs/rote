cd73760d519b1b4fab83488b99f3345a1bdfb570
Chris Brower <cbrower@lavabit.com>
2020-09-02 22:00:43 -0500

    add math shortcode

--- static/js/rote.js
+++ static/js/rote.js
@@ -1,3 +1,17 @@
 // Load Mermaid to render charts
 mermaid.initialize({startOnLoad:true});
 
+// MathJax Configuration
+MathJax = {
+  svg: {
+    displayAlign: 'left'
+  }
+};
+
+// Load MathJax
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
